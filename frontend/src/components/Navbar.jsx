22403738ae3419e0533826c07cbcf57ca1db9ee6
vimal <vimalonboard69@gmail.com>
2025-01-13 18:16:23 +0530

    colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
       <img className='w-44 cursor-pointer' src= {assets.logo} alt ="" />
       <ul className='hidden md:flex items-center font-medium justify-between w-1/2'>
         <NavLink>
-            <li>Home</li>
-            <hr/>
+            <li className='py-1' >Home</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/4 m-auto'/>
         </NavLink>
         <NavLink>
-            <li>All Doctors</li>
-            <hr/>
+            <li className='py-1'>All Doctors</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/4 m-auto'/>
         </NavLink>
         <NavLink>
-            <li>About</li>
-            <hr/>
+            <li className='py-1'>About</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/4 m-auto'/>
         </NavLink>
         <NavLink>
-            <li>Contact</li>
-            <hr/>
+            <li className='py-1'>Contact</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/4 m-auto'/>
         </NavLink>
       </ul>
       <div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+        'danger': '#FF6F61',
+
+
+      }
+    },
   },
   plugins: [],
 }
