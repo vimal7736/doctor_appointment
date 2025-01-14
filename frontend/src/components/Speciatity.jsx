3a63d4c662b12e727a65c69fde65b82849b32dcb
vimal <vimalonboard69@gmail.com>
2025-01-14 18:43:39 +0530

    updated

--- frontend/src/components/Speciatity.jsx
+++ frontend/src/components/Speciatity.jsx
@@ -4,18 +4,18 @@ import { specialityData } from '../assets/assets'
 const Speciatity = () => {
     return (
         <div className='p-4 flex items-center flex-col' id='speciality'>
-            <div className='text-2xl font-bold'>
-                Speciality
+            <div className='text-3xl font-extrabold text-primary mb-4'>
+                Our Specialities
             </div>
             <p className='text-[10px] text-center md:text-base lg:text-lg'>
                 Discover specialized care tailored to your needs. From pediatrics to cardiology, dermatology to orthopedics, our platform connects you to top medical professionals in every field. Your health deserves expert attention—schedule your consultation today and experience compassionate, comprehensive care
             </p>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 '>
                 {specialityData.map((data) => (
-                    <div key={data.id} className='bg-gray-100 p-4 m-4 rounded-md'>
-                        <img src={data.image} alt={data.title} className='w-20 h-20' />
-                        <h2 className='text-xl font-bold'>{data.speciality}</h2>
-                        <p className='text-sm'>{data.description}</p>
+                    <div key={data.id} className='items-center flex flex-col bg-gradient-to-r from-gray-700 via-gray-900 to-primary text-gray-400 p-4 m-4 rounded-sm'>
+                        <img src={data.image} alt={data.title} className='w-20 h-20  rounded-full' />
+                        <h2 className='text-xl text-gray-300 font-bold'>{data.speciality}</h2>
+                        <p className=' text-center text-[12px]'>{data.description}</p>
                     </div>
                 ))}
             </div>
