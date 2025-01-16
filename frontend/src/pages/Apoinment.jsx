8e44dce0f0421caf4d96c7aedfd69fa68e0712e1
vimal <vimalonboard69@gmail.com>
2025-01-16 19:59:27 +0530

    appoinment page

--- frontend/src/pages/Apoinment.jsx
+++ frontend/src/pages/Apoinment.jsx
@@ -52,29 +52,29 @@ const Apoinment = () => {
 
   return (
     <div className='px-2 py-4 '>
-      <div className='flex flex-wrap flex-col md:flex-row justify-start items-center'>
-        <div className='border-2 w-1/4 bg-gray-800'>
+      <div className='flex flex-wrap flex-col md:flex-row justify-start  mt-4'>
+        <div className='border-2 w-1/4  bg-blue-500'>
           <img className='' src={doctor.image} alt="" />
         </div>
-        <div className='border-2 w-3/4 flex flex-col  '>
-          <div><span className='font-semibold'>Name:</span> {doctor.name}</div>
-          <div><span className='font-semibold'>Degree:</span>{doctor.degree}</div>
-          <div><span className='font-semibold' >Experience:</span>{doctor.experience}</div>
-          <div><span className='font-semibold'>specializd:</span>{doctor.speciality}</div>
-          <div><span className='font-semibold font-roboto'>fee:</span>{currency}{doctor.fees}</div>
+        <div className='p-3 w-3/4 flex flex-col  '>
+          <div><span className='font-semibold text-center  '>Name: </span> {doctor.name}</div>
+          <div><span className='font-semibold text-center '>Degree: </span>{doctor.degree}</div>
+          <div><span className='font-semibold text-center ' >Experience: </span>{doctor.experience}</div>
+          <div><span className='font-semibold text-center '>specializd: </span>{doctor.speciality}</div>
+          <div><span className='font-mono text-center border-2 px-2 border-danger   rounded-full'>fee</span> : {currency}{doctor.fees}</div>
 
         </div>
       </div>
-      <div>
+      <div className='mt-4'>
         {dates && dates.map((item, index) => (
           <div key={index}>
-            <p>
+            <p className='font-semibold text-xl border w-full rounded-full bg-slate-800 text-white mt-4 px-4'>
               {item.date}
             </p>
-            <div className='flex gap-5 flex-wrap'>
+            <div className='flex gap-5 flex-wrap mt-4'>
               {
-                item.timeSlots.map(time=>(
-                  <span>{time}</span>
+                item.timeSlots.map(time => (
+                  <span className='border-2 rounded-full p-2'>{time}</span>
                 ))
               }
             </div>
