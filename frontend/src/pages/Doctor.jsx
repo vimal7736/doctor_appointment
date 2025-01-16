3603f84f1805b3bf9d9fdae76c3302f46eae87f7
vimal <vimalonboard69@gmail.com>
2025-01-16 14:42:19 +0530

    small update

--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -28,11 +28,11 @@ const Doctor = () => {
 
   return (
     <div className='p-2 flex flex-col gap-4 '>
-      <p className=' px-6 text-pretty font-poppins font-semibold'>Connect with experienced doctors for your healthcare needs</p>
+      <p className=' px-6 text-pretty font-poppins text-md md:text-lg lg:text-3xl font-semibold'>Connect with experienced doctors for your healthcare needs</p>
       <div className='flex flex-col items-center md:flex-col justify-between '>
         <div className="flex flex-wrap justify-between py-4 px-6 gap-4">
           <p
-            className="cursor-pointer border-2 border-gray-300 px-4 py-2 text-xs md:text-sm font-medium rounded-full hover:bg-danger/10 transition-all duration-300"
+            className="cursor-pointer border-2 border-gray-300 px-4 py-2 text-xs md:text-sm font-medium rounded-full hover:bg-danger transition-all duration-300"
             onClick={() => speciality === '' ? nav('/doctors') : nav('/doctors')}
           >
             All Doctors
@@ -48,10 +48,10 @@ const Doctor = () => {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
           {filteredDoctors.map((doctor) => (
             <div
-              className="border border-gray-200 shadow-sm rounded-lg p-4 bg-white hover:shadow-md transition-shadow duration-300 flex flex-col items-center text-center"
+              className=" transition-all hover:scale-90  border border-gray-200 shadow-sm rounded-lg p-4 bg-white hover:shadow-md  duration-300 flex flex-col items-center text-center"
               key={doctor._id}
             >
               <div className="w-24 h-24 rounded-full overflow-hidden shadow-sm mb-4">
