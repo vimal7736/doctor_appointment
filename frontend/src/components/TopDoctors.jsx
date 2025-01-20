1a623f3e4e783750335e6368bc54ff524a17db93
vimal <vimalonboard69@gmail.com>
2025-01-20 19:05:03 +0530

    css

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -16,8 +16,8 @@ const TopDoctors = () => {
                         <p className='text-sm text-gray-600 mb-6'>When it comes to your health, trust only the best</p>
                         <div className=' relative w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                                 {doctors && doctors.slice(0, 8).map((doctor) => (
-                                        <div onClick={() => navigate(`/appointment/${doctor._id}`)} key={doctor._id} className='cursor-pointer text-white shadow-lg  duration-300 p-4 flex flex-col items-center bg-primary rounded-md hover:shadow-sm hover:shadow-slate-700 transition-all  md:scale-75 md:hover:scale-100 '>
-                                                <p className='text-sm text-white  font-semibold '>{doctor.name}</p>
+                                        <div onClick={() => navigate(`/appointment/${doctor._id}`)} key={doctor._id} className='cursor-pointer text-slate-700 shadow-lg  duration-300 p-4 flex flex-col items-center bg-white rounded-md hover:shadow-lg hover:shadow-slate-700 transition-all  md:scale-90 md:hover:scale-110 '>
+                                                <p className='text-sm  font-semibold '>{doctor.name}</p>
                                                 <div className='w-24 rounded-full h-24 bg-primary border-2  '>
                                                         <img
                                                                 className='rounded-full shadow-md shadow-white'
@@ -26,7 +26,7 @@ const TopDoctors = () => {
                                                         />
 
                                                 </div>
-                                                <p className='text-sm text-gray-50'>{doctor.speciality}</p>
+                                                <p className='text-sm '>{doctor.speciality}</p>
                                                 <div className='flex items-center  gap-2 p-2'>
                                                         <div className='w-2 h-2 rounded bg-green-500' ></div>
                                                         <div>Available</div>
@@ -38,7 +38,7 @@ const TopDoctors = () => {
                         <br />
                         <button
                                 onClick={() => { navigate('/doctors'); scrollTo(0, 0) }}
-                                className='bg-gray-800 mt-2 text-sm font-medium w-1/6 h-10 text-white rounded-full hover:bg-primary'>More</button>
+                                className='bg-white mt-2 text-sm font-medium w-1/6 h-10 text-slate-800 shadow-xl rounded-full hover:text-white hover:bg-primary'>More</button>
                         <br />
                         <br />
                 </div>
